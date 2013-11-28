b6199cbde004787c8da2497e37f2aee217ee3197
Michael Hayes <michael.hayes@urbanairship.com>
2013-11-27 19:10:44 -0800

    fasd

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var el = document.getElementById('main')
   , Template = require('./lib/index')
   , template = Template(el)
+  , state1 = {}
+  , state2 = {}
+  , odd = true
 
-var state = {
-    a: {b: 10}
-  , show: false
-  , dont: true
-  , items: [4,5]
-}
+state1.a = {b: 5}
+state1.show = true
+state1.dont = false
+state1.items = [1,2,3]
 
-template.write(state)
+state2.a = {b: 10}
+state2.show = true
+state2.dont = false
+state2.items = [4,5,6]
+template.write(state1)
 
-setTimeout(function() {
-  state.a.b = 5
-  state.show = true
-  state.dont = false
-  state.items = [1,2,3]
-  template.write(state)
-}, 1000)
+setInterval(function() {
+  odd = !odd
+
+  template.write(odd ? state1 : state2)
+},0)
