e0c01f66f0c76acc02247686c2c87413f04f9db9
Michael Hayes <michael.hayes@urbanairship.com>
2014-03-19 20:04:48 -0700

    no more deps, added with, filters are no streams

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var el = document.getElementById('main')
-  , Template = require('./lib/index')
-  , template = Template(el)
+  , altr = require('./lib/index')
+  , template = altr(el)
 
 var state = {
     a: {b: 10}
@@ -8,17 +8,24 @@ var state = {
   , dont: true
   , items: [4,5]
   , attr: 'attrs work'
-  , value: 'this was inserted by the value tag'
+  , text: 'this was inserted by the text tag'
   , html: '<strong>inserted by the html tag</strong>'
   , num: 5
+  , num2: 11
+  , color: 'red'
 }
 
-template.write(state)
+template.update(state)
+
+var state2 = Object.create(state)
+
+state2.a.b = 5
+state2.show = true
+state2.dont = false
+state2.items = [1,2,3]
+
+var counter = 0
 
 setTimeout(function() {
-  state.a.b = 5
-  state.show = true
-  state.dont = false
-  state.items = [1,2,3]
-  template.write(state)
-}, 1000)
+  template.update(++counter % 2 ? state2 : state)
+}, 0)
