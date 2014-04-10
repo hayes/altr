12257c090c30cb8a62a3333f8343c2d221d80438
Michael Hayes <michael.hayes@urbanairship.com>
2014-04-10 14:39:00 -0700

    animate graph

--- pages/demos/graph.js
+++ pages/demos/graph.js
@@ -1,11 +1,24 @@
+var ease = require('ease-component')
+
 altr.add_filter('svg_path', svg_path)
+altr.add_filter('ease_array', ease_array)
 altr.add_filter('spread', spread)
 
 var data = {
-    points: [50, 30, 60, 80, 10, 40, 15, 60, 40, 50, 70, 90, 30, 15, 10, 11]
+    points: []
 }
 
-altr(document.getElementById('graph1'), data)
+var template = altr(document.getElementById('graph1'), data)
+
+setInterval(function() {
+  data.points = []
+
+  for(var i = 0; i < 20; ++i) {
+    data.points.push(~~(Math.random() * 100))
+  }
+
+  template.update(data)
+}, 1000)
 
 function spread(range, change) {
   range = +range
@@ -28,5 +41,42 @@ function svg_path(parts, change) {
     })))
 
     change(result.join(' '))
+  }
+}
+
+function ease_array(parts, change) {
+  var target = []
+    , prev = []
+    , timer
+    , start
+
+  parts = parts.split(',')
+
+  var fn = parts[1] || 'linear'
+    , ms = +parts[0]
+
+  return update
+
+  function update(d) {
+    clearTimeout(timer)
+    timer = setTimeout(animate, 5)
+    prev = target
+    target = d || target
+    start = new Date
+  }
+
+  function animate() {
+    var diff = new Date - start
+      , p = diff / ms
+
+    if(p >= 1) {
+      p = 1
+    } else {
+      timer = setTimeout(animate, 5)
+    }
+
+    change(target.map(function(d, i) {
+      return prev[i] + (target[i] - (prev[i] || 0)) * p
+    }))
   }
 }
