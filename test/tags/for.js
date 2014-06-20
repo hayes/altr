2eeb3ade3cc9b83bcccbdcc2fc3c916fc8695290
Michael Hayes <michael.hayes@urbanairship.com>
2014-06-20 10:51:39 -0700

    more checks less updates in for loop

--- test/tags/for.js
+++ test/tags/for.js
@@ -85,3 +85,39 @@ test('for respects placeholder', function(t) {
       '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
   )
 })
+
+test('adding and removing', function(t) {
+  var template = altr(
+      '<ul altr-for="item in items"><li altr-if="show">{{ item }}</li></ul>'
+    , {show: true, items: [1,2,3]}
+  )
+
+  t.plan(4)
+  t.equal(
+      template.toString()
+    , '<ul altr-for="item in items"><li altr-if="show">1</li>' +
+      '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
+  )
+
+  template.update({show: false, items: [4,3,2,1]})
+  t.equal(
+      template.toString()
+    , '<ul altr-for="item in items"><!--altr-if-placeholder-->' +
+      '<!--altr-if-placeholder--><!--altr-if-placeholder-->' +
+      '<!--altr-if-placeholder--></ul>'
+  )
+
+  template.update({show: true, items: [3,4,1]})
+  t.equal(
+      template.toString()
+    , '<ul altr-for="item in items"><li altr-if="show">3</li>' +
+      '<li altr-if="show">4</li><li altr-if="show">1</li></ul>'
+  )
+
+  template.update({show: true, items: [4,5,6]})
+  t.equal(
+      template.toString()
+    , '<ul altr-for="item in items"><li altr-if="show">4</li>' +
+      '<li altr-if="show">5</li><li altr-if="show">6</li></ul>'
+  )
+})
