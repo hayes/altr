1528dadeee9cf859f8c3fbd5a748f2487f35cbf9
Michael Hayes <michael.hayes@urbanairship.com>
2014-07-16 12:44:36 -0700

    better attrs

--- test/attributes.js
+++ test/attributes.js
@@ -3,22 +3,37 @@ var altr = require('../lib')
 
 test('basic attributes', function(t) {
   var template = altr(
-      '<h1 class="{{ class }}" quote="{{ q }}">text</h1>'
+      '<h1 class="{{ class }}" quote="{{ q }}{{ \'!\' }}">text</h1>'
     , {class: 'big', q: '"'}
     , true
   )
 
   t.plan(1)
-  t.equal(template.toString(), '<h1 class="big" quote="&quot;">text</h1>')
+  t.equal(template.toString(), '<h1 class="big" quote="&quot;!">text</h1>')
 })
 
 test('altr attributes', function(t) {
   var template = altr(
-      '<h1 altr-attr-class="{{ class }}">text</h1>'
+      '<h1 altr-attr-class="class">text</h1>'
     , {class: 'big'}
     , true
   )
 
   t.plan(1)
   t.equal(template.toString(), '<h1 class="big">text</h1>')
+})
+
+test('remove attributes', function(t) {
+  var template = altr(
+      '<h1 altr-attr-class="class">text</h1>'
+    , {class: null}
+    , true
+  )
+
+  t.plan(3)
+  t.equal(template.toString(), '<h1>text</h1>')
+  template.update({class: 'big'})
+  t.equal(template.toString(), '<h1 class="big">text</h1>')
+  template.update({})
+  t.equal(template.toString(), '<h1>text</h1>')
 })
