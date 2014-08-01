70a7ad572c73b654b9a385907992e7d753ea9342
Michael Hayes <michael.hayes@urbanairship.com>
2014-08-01 12:03:21 -0700

    directive tests

---
+++ test/directives.js
@@ -0,0 +1,64 @@
+var altr = require('../lib')
+  , test = require('tape')
+
+test('directive', function(t) {
+  altr.addDirective('foo-dir', foo)
+
+  t.plan(2)
+
+  var template = altr(
+      '<div foo-dir="name, the.value">'
+    , {name: 'world', the: {value: 10}}
+    , true
+  )
+
+  function foo(el) {
+    return {update: update}
+
+    function update(name, val) {
+      t.equal(val, 10)
+      t.equal(name, 'world')
+    }
+  }
+})
+
+test('destroy directive', function(t) {
+  altr.addDirective('destroy', destroy)
+
+  var template = altr(
+      '<ul altr-for="item in items"><li destroy="item"></li></ul>'
+    , {items: [1,2,3]}
+    , true
+  )
+
+  t.plan(6)
+  template.update()
+  t.end()
+
+  function destroy(el) {
+    t.ok(true)
+
+    return {destroy: t.ok.bind(t, true)}
+  }
+})
+
+test('nested for destroy directive', function(t) {
+  altr.addDirective('nested-destroy', destroy)
+
+  var template = altr(
+      '<ul altr-for="outer in items"><li altr-for="inner in outer">' +
+      '<div nested-destroy="item"></div></li></ul>'
+    , {items: [[1,2,3]]}
+    , true
+  )
+
+  t.plan(6)
+  template.update()
+  t.end()
+
+  function destroy(el) {
+    t.ok(true)
+
+    return {destroy: t.ok.bind(t, true)}
+  }
+})
