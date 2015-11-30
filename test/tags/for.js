55cffc4269873466fa97bd6e5cf8c4746a65e5a5
Michael Hayes <mhayes@newrelic.com>
2015-11-29 17:22:45 -0800

    restored some tests and updated build tools

--- test/tags/for.js
+++ test/tags/for.js
@@ -51,14 +51,14 @@ test('nested fors', function (t) {
   t.equal(
     template.toString(),
     '<ul altr-for="o in out"><ul altr-for="i in in">a1a2</ul>' +
-      '<ul altr-for="i in in">b1b2</ul></ul>'
+    '<ul altr-for="i in in">b1b2</ul></ul>'
   )
 })
-/*
-test('for respects placeholder', function(t) {
+
+test('for respects placeholder', function (t) {
   var template = altr(
     '<ul altr-for="item in items"><li altr-if="show">{{ item }}</li></ul>',
-  {show: true, items: [1,2,3]},
+    {show: true, items: [1, 2, 3]},
     {sync: true}
   )
 
@@ -69,32 +69,32 @@ test('for respects placeholder', function(t) {
       '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
   )
 
-  template.update({show: false, items: [3,2,1]})
+  template.update({show: false, items: [3, 2, 1]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><!--altr-if-placeholder-->' +
-      '<!--altr-if-placeholder--><!--altr-if-placeholder--></ul>'
+    '<!--altr-if-placeholder--><!--altr-if-placeholder--></ul>'
   )
 
-  template.update({show: true, items: [3,2,1]})
+  template.update({show: true, items: [3, 2, 1]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><li altr-if="show">3</li>' +
-      '<li altr-if="show">2</li><li altr-if="show">1</li></ul>'
+    '<li altr-if="show">2</li><li altr-if="show">1</li></ul>'
   )
 
-  template.update({show: true, items: [1,2,3]})
+  template.update({show: true, items: [1, 2, 3]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><li altr-if="show">1</li>' +
-      '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
+    '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
   )
 })
 
-test('adding and removing', function(t) {
+test('adding and removing', function (t) {
   var template = altr(
     '<ul altr-for="item in items"><li altr-if="show">{{ item }}</li></ul>',
-  {show: true, items: [1,2,3]},
+    {show: true, items: [1, 2, 3]},
     {sync: true}
   )
 
@@ -102,29 +102,28 @@ test('adding and removing', function(t) {
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><li altr-if="show">1</li>' +
-      '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
+    '<li altr-if="show">2</li><li altr-if="show">3</li></ul>'
   )
 
-  template.update({show: false, items: [4,3,2,1]})
+  template.update({show: false, items: [4, 3, 2, 1]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><!--altr-if-placeholder-->' +
-      '<!--altr-if-placeholder--><!--altr-if-placeholder-->' +
-      '<!--altr-if-placeholder--></ul>'
+    '<!--altr-if-placeholder--><!--altr-if-placeholder-->' +
+    '<!--altr-if-placeholder--></ul>'
   )
 
-  template.update({show: true, items: [3,4,1]})
+  template.update({show: true, items: [3, 4, 1]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><li altr-if="show">3</li>' +
-      '<li altr-if="show">4</li><li altr-if="show">1</li></ul>'
+    '<li altr-if="show">4</li><li altr-if="show">1</li></ul>'
   )
 
-  template.update({show: true, items: [4,5,6]})
+  template.update({show: true, items: [4, 5, 6]})
   t.equal(
     template.toString(),
     '<ul altr-for="item in items"><li altr-if="show">4</li>' +
-      '<li altr-if="show">5</li><li altr-if="show">6</li></ul>'
+    '<li altr-if="show">5</li><li altr-if="show">6</li></ul>'
   )
 })
-*/
